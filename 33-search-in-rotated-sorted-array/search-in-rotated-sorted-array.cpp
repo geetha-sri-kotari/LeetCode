3a03567547085309fee9a9863b36df5eb0202bec
Geetha Sri Kotari <23A91A0529@aec.edu.in>
2025-09-19 09:33:47 +0530

    Time: 0 ms (100.00%) | Memory: 15.1 MB (94.57%) - LeetSync

---
+++ 33-search-in-rotated-sorted-array/search-in-rotated-sorted-array.cpp
@@ -0,0 +1,10 @@
+class Solution {
+public:
+    int search(vector<int>& nums, int target) {
+        //int idx = -1;
+        for(int i=0; i<nums.size(); i++) {
+            if(nums[i] == target) return i;
+        }
+        return -1;
+    }
+};
