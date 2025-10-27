6593a2dc26c494955d7e36e7365318b01fe01a6c
Geetha Sri Kotari <23A91A0529@aec.edu.in>
2025-10-27 20:27:01 +0530

    Time: 2881 ms (5.04%) | Memory: 22 MB (51.40%) - LeetSync

---
+++ 322-coin-change/coin-change.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<long long> dp(amount+1, -1);
+        dp[0] = 0;
+        for(long long i=1; i<=amount; i++) {
+            long long min_coins = INT_MAX;
+            long long left = 1, right = i-1;
+            while(left <= right) {
+                if(dp[left] > 0 && dp[right] > 0)
+                min_coins = min(min_coins, dp[left] + dp[right]); 
+                left++;
+                right--;
+            }
+            for(long long j=0; j<coins.size(); j++) {
+                if(i % coins[j] == 0) 
+                min_coins = min(min_coins, i/coins[j]);
+            }
+            if(min_coins != INT_MAX) dp[i] = min_coins;
+        }
+        //for(int i=0; i<dp.size(); i++) cout << dp[i] << " ";
+        return dp[amount];
+    }
+};
